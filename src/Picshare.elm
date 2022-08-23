877d08a186ca041e7ef2586d9bd901da1b2c9bac
Cai Gao <cgao@corvusinsurance.com>
2022-08-23 18:26:27 -0400

    Refactor like button

--- src/Picshare.elm
+++ src/Picshare.elm
@@ -26,8 +26,8 @@ initialModel =
     }
 
 
-viewDetailedPhoto : Model -> Html Msg
-viewDetailedPhoto model =
+viewLoveButton : Model -> Html Msg
+viewLoveButton model =
     let
         buttonClass =
             if model.liked then
@@ -35,25 +35,23 @@ viewDetailedPhoto model =
 
             else
                 "fa-heart-o"
-
-        msg =
-            if model.liked then
-                Unlike
-
-            else
-                Like
     in
+    div [ class "like-button" ]
+        [ i
+            [ class " fa fa-2x"
+            , class buttonClass
+            , onClick ToggleLike
+            ]
+            []
+        ]
+
+
+viewDetailedPhoto : Model -> Html Msg
+viewDetailedPhoto model =
     div [ class "detailed-photo" ]
         [ img [ src model.url ] []
         , div [ class "photo-info" ]
-            [ div [ class "like-button" ]
-                [ i
-                    [ class " fa fa-2x"
-                    , class buttonClass
-                    , onClick msg
-                    ]
-                    []
-                ]
+            [ viewLoveButton model
             , h2 [ class "caption" ] [ text model.caption ]
             ]
         ]
@@ -70,18 +68,14 @@ view model =
 
 
 type Msg
-    = Like
-    | Unlike
+    = ToggleLike
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Like ->
-            { model | liked = True }
-
-        Unlike ->
-            { model | liked = False }
+        ToggleLike ->
+            { model | liked = not model.liked }
 
 
 main : Program () { url : String, caption : String, liked : Bool } Msg
